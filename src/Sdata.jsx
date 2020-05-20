6bfeb92f1f6eb1f485c92a2ff164ef65650a9bde
ayushshrivastava28 <ayushshrivastava28@gmail.com>
2020-05-20 19:04:46 +0530

    firstcommit

---
+++ src/Sdata.jsx
@@ -0,0 +1,33 @@
+const sdata=[
+    {   
+        sname:"PUNISHER",
+        imgsrc:"https://resizing.flixster.com/OH5QcrZNHPAd_ofrKi22DiuiS7k=/206x305/v1.dDs0MDc5NjA7ajsxODQwOTsxMjAwOzEwNjY7MTU5OQ" ,
+        title:"A Netflix Series",
+        link:"https://www.netflix.com/title/80117498",
+    },
+    {
+        sname:"STRANGER THINGS",
+        imgsrc:"https://wallpapercave.com/wp/wp1839578.jpg" ,
+        title:"A Netflix Series",
+        link:"https://www.netflix.com/title/80057281",
+    },
+    {
+        sname:"DARK",
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg" ,
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/title/80100172",
+    },
+    {   
+        sname:"VIKINGS",
+        imgsrc:"https://wallpapercave.com/wp/wp2934426.jpg" ,
+        title:"A Netflix Series",
+        link:"https://www.netflix.com/title/70301870",
+    },
+    {   
+        sname:"BREAKING BAD",
+        imgsrc:"https://wallpapercave.com/wp/thq0mll.jpg" ,
+        title:"A Netflix Series",
+        link:"https://www.netflix.com/title/70143836",
+    }
+]
+export default sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Card from './Cards';
+import Sdata from './Sdata';
+import "./style.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+<>
+<h1 className="heading_style">List of top 5 Netflix Series</h1>
+  <Card imgsrc={Sdata[0].imgsrc} 
+  title={Sdata[0].title}
+  sname={Sdata[0].sname}
+  link={Sdata[0].link}
+  />
+  <Card imgsrc={Sdata[1].imgsrc} 
+  title={Sdata[1].title}
+  sname={Sdata[1].sname}
+  link={Sdata[1].link}
+  />
+  <Card imgsrc={Sdata[2].imgsrc} 
+  title={Sdata[2].title}
+  sname={Sdata[2].sname}
+  link={Sdata[2].link}
+  />
+  <Card imgsrc={Sdata[3].imgsrc} 
+  title={Sdata[3].title}
+  sname={Sdata[3].sname}
+  link={Sdata[3].link}
+  />
+  <Card imgsrc={Sdata[4].imgsrc} 
+  title={Sdata[4].title}
+  sname={Sdata[4].sname}
+  link={Sdata[4].link}
+  />
+</>,
+document.getElementById("root"));
